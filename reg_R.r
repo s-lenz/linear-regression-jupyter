71f68b645ecb5b1ff205380a3201acc48805af51
Sidney Lenz <slenz@ascend-rw01.ten.osc.edu>
2025-06-24 20:44:53 -0400

    added scripts + resulting graphs

---
+++ reg_R.r
@@ -0,0 +1,23 @@
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) != 3) {
+  stop("Usage: Rscript linear_regression_r.R <filename> <x_column> <y_column>")
+}
+
+filename <- args[1]
+x_col <- args[2]
+y_col <- args[3]
+
+data <- read.csv(filename)
+formula <- as.formula(paste(y_col, "~", x_col))
+model <- lm(formula, data = data)
+
+library(ggplot2)
+plot <- ggplot(data, aes(x = !!sym(x_col), y = !!sym(y_col))) +
+  geom_point(color = "red") +
+  geom_smooth(method = "lm", color = "blue") +
+  ggtitle(paste(y_col, "vs", x_col)) +
+  xlab(x_col) +
+  ylab(y_col)
+
+ggsave("linear_regression_r_output.png", plot)
+print(plot)
---
+++ reg_py.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+if len(sys.argv) != 4:
+    print("Usage: python linear_regression_python.py <filename> <x_column> <y_column>")
+    sys.exit(1)
+
+filename = sys.argv[1]
+x_col = sys.argv[2]
+y_col = sys.argv[3]
+
+data = pd.read_csv(filename)
+model = LinearRegression()
+model.fit(data[[x_col]], data[[y_col]])
+
+plt.plot(data[[x_col]], model.predict(data[[x_col]]), color='blue')
+plt.scatter(data[[x_col]], data[[y_col]], color='red')
+plt.title(f'{y_col} vs {x_col}')
+plt.xlabel(x_col)
+plt.ylabel(y_col)
+plt.savefig("linear_regression_python_output.png")
+plt.show()
+
+
